d292b7ca621cfbe34a2c853f58c369bc1114bf36
UY-Scuti-Real <37155328+UY-Scuti-Real@users.noreply.github.com>
2019-06-13 01:13:24 +0100

    remove individual functions for servos implement servo classes for easy calibration & setup move mappings to module variables add null map change timeout behaviour: null map now applied

--- electronicsdriver.py
+++ electronicsdriver.py
@@ -37,130 +37,56 @@ an, you know the dril it's the same as before
 if the rover has debug LEDs/sounds, should be presented as
 on, output number respectively
 """
-kit = ServoKit(channels=16)
-
-def servo_angle(spd):
-    try:
-        angle = float(spd)*80 + 90
-        kit.servo[0].angle = angle
-        #print("angle should be ", angle)
-    except Exception as e:
-        print("But that's wrong, you fucking retard: ", e, spd)
 
 
-def debug_cmd(msg):
-    pass
+class servo:
+    def __init__(self, channel, min_pulse=0, max_pulse=0):
+        self.channel = channel
+        self.min_pulse = min_pulse
+        self.max_pulse = max_pulse
+
+    def __call__(self, val):
+        pass
+
+    def calibrate(self):
+        pass
 
 
-def wheel1(spd):
-    try:
-        angle = -float(spd)
-        kit.continuous_servo[0].throttle = angle
-        #print("angle should be ", angle)
-    except Exception as e:
-        print("wheel1: ", e, spd)
+class continous_servo(servo):
+    def __init__(self, channel, min_pulse=0, max_pulse=0):
+        # might be from python 2, keeping for austerity
+        # super(continous_servo, self).__init__(channel, min_pulse, max_pulse)
+        super().__init__(self)
+        if min_pulse and max_pulse:
+            self.calibrate()
 
-def wheel2(spd):
-    try:
-        angle = float(spd)
-        kit.continuous_servo[1].throttle = angle
-        #print("angle should be ", angle)
-    except Exception as e:
-        print("wheel2: ", e, spd)
+    def __call__(self, throttle):
+        if -1 <= throttle <= 1:
+            kit.continous_servo[self.channel].throttle = throttle
+        else:
+            pass
 
-
-def wheel3(spd):
-    try:
-        angle = -float(spd)
-        kit.continuous_servo[2].throttle = angle
-        #print("angle should be ", angle)
-    except Exception as e:
-        print("wheel3: ", e, spd)
+    def calibrate(self):
+        kit.continous_servo[self.channel].set_pulse_width_range(self.min_pulse,
+                                                                self.max_pulse)
 
 
-def wheel4(spd):
-    try:
-        angle = float(spd)
-        kit.continuous_servo[3].throttle = angle
-        #print("angle should be ", angle)
-    except Exception as e:
-        print("wheel4: ", e, spd)
+class standard_servo(servo):
+    def __init__(self, channel, min_pulse=0, max_pulse=0):
+        # super(standard_servo, self).__init__(channel, min_pulse, max_pulse)
+        super().__init__(self)
+        if min_pulse and max_pulse:
+            self.calibrate()
 
+    def __call__(self, angle):
+        if 0 <= angle <= 180:
+            kit.servo[self.channel].angle = angle
+        else:
+            pass
 
-def wheel5(spd):
-    try:
-        angle = -float(spd)
-        kit.continuous_servo[4].throttle = angle
-        #print("angle should be ", angle)
-    except Exception as e:
-        print("wheel5: ", e, spd)
-
-
-def wheel6(spd):
-    try:
-        angle = float(spd)
-        kit.continuous_servo[5].throttle = angle
-        #print("angle should be ", angle)
-    except Exception as e:
-        print("wheel6: ", e, spd)
-
-
-def gimbal1(spd):
-    try:
-        angle = float(spd)*90+90
-        kit.servo[6].angle = angle
-        #print("angle should be ", angle)
-    except Exception as e:
-        print("gimbal1: ", e, spd)
-
-
-def gimbal2(spd):
-    try:
-        angle = float(spd)*90+90
-        kit.servo[7].angle = angle
-        #print("angle should be ", angle)
-    except Exception as e:
-        print("gimbal2: ", e, spd)
-
-
-def gimbal3(spd):
-    try:
-        angle = float(spd)*90+90
-        kit.servo[8].angle = angle
-        #print("angle should be ", angle)
-    except Exception as e:
-        print("gimbal3: ", e, spd)
-
-
-def gimbal4(spd):
-    try:
-        angle = float(spd)*90+90
-        kit.servo[9].angle = angle
-        #print("angle should be ", angle)
-    except Exception as e:
-        print("gimbal4: ", e, spd)
-
-
-def shoulder(spd):
-    try:
-        kit.servo[10].angle = float(spd)
-        #print("shoulder", spd, end = '')
-    except Exception as e:
-        print("But that's wrong, you fucking shoulder: ", e, spd)
-
-def elbow(spd):
-    try:
-        kit.servo[11].angle = float(spd)
-        #print("\t elbow", spd, "\n", end = '\n')
-    except Exception as e:
-        print("But that's wrong, you fucking elbow: ", e, spd)
-
-
-def grabber(spd):
-    try:
-        kit.servo[12].angle = float(spd)
-    except Exception as e:
-        print("But that's wrong, you fucking grabber: ", e, spd)
+    def calibrate(self):
+        kit.servo[self.channel].set_pulse_width_range(self.min_pulse,
+                                                      self.max_pulse)
 
 
 class driver:
@@ -171,39 +97,9 @@ class driver:
         self.command_sock.bind(("192.168.1.10", 5000))
         self.command_sock.listen(2048)
         if mode == 0:
-            self.map = {"w1":print,
-                        "w2":print,
-                        "w3":print,
-                        "w4":print,
-                        "w5":print,
-                        "w6":print,
-                        "g1":print,
-                        "g2":print,
-                        "g3":print,
-                        "g4":print,
-                        "g5":print,
-                        "g6":print,
-                        "a1":print,
-                        "a2":print,
-                        "a3":print,
-                        }
-        if mode == 1:
-            self.map = {"w1":wheel1,
-                        "w2":wheel2,
-                        "w3":wheel3,
-                        "w4":wheel4,
-                        "w5":wheel5,
-                        "w6":wheel6,
-                        "g1":gimbal1,
-                        "g2":gimbal2,
-                        "g3":gimbal3,
-                        "g4":gimbal4,
-                        "g5":debug_cmd,
-                        "g6":debug_cmd,
-                        "a1":shoulder,
-                        "a2":elbow,
-                        "a3":grabber,
-                        }
+            self.map = print_map
+        elif mode == 1:
+            self.map = electronics_map
         while 1:
             self.aquire_conn()
             while self.connection:
@@ -211,15 +107,12 @@ class driver:
                 if msg and msg != b'1':
                     speeds = self.convert_msg(msg)
                     self.update(speeds)
-                    
-                
-                    
+
     def aquire_conn(self):
         self.command_sock.setblocking(1)
         self.connection, addr = self.command_sock.accept()
         print("got connection from {}".format(addr))
         self.command_sock.setblocking(0)
-        
 
     def get_msg(self):
         try:
@@ -238,9 +131,8 @@ class driver:
                 return msg.decode()
         except ConnectionResetError:
             return False
-        
+
     def convert_msg(self, msg):
-##        try:
         aryform = str(msg).split(",")
         dictform = {}
         for ary in aryform:
@@ -249,21 +141,83 @@ class driver:
             key, value = ary.split(":")
             dictform[key] = value
         return dictform
-##        except ValueError as e:
-            #print("oppsie:", e , "\n", aryform)
-##            pass
 
     def update(self, speeds):
-        #this feels very wrong
         if speeds is not None:
             for speed in speeds:
                 if speed in self.map:
                     self.map[speed](speeds[speed])
-##            if "a1" in speeds:
-##                print("a1 ",speeds["a1"])
-##            if "a2" in speeds:
-##                print("a2", speeds["a2"])
-            #print(speeds)
+
+
+# MAIN ==============================================================
+kit = ServoKit(channels=16)
+# wheel declartions (calibration needed)
+wheel1 = continous_servo(0)
+wheel2 = continous_servo(1)
+wheel3 = continous_servo(2)
+wheel4 = continous_servo(3)
+wheel5 = continous_servo(4)
+wheel6 = continous_servo(5)
+# gimbal declarations (calibration needed)
+gimbal1 = standard_servo(6)
+gimbal2 = standard_servo(7)
+gimbal3 = standard_servo(8)
+gimbal4 = standard_servo(9)
+# arm declartions (calibration needed)
+shoulder = standard_servo(10)
+elbow = standard_servo(11)
+grabber = standard_servo(12)
+waist = standard_servo(13)
+cargo = standard_servo(14)
+
+print_map = {"w1": print,
+             "w2": print,
+             "w3": print,
+             "w4": print,
+             "w5": print,
+             "w6": print,
+             "g1": print,
+             "g2": print,
+             "g3": print,
+             "g4": print,
+             "a1": print,
+             "a2": print,
+             "a3": print,
+             "a4": print,
+             "p4": print,
+             }
+
+electronics_map = {"w1": wheel1,
+                   "w2": wheel2,
+                   "w3": wheel3,
+                   "w4": wheel4,
+                   "w5": wheel5,
+                   "w6": wheel6,
+                   "g1": gimbal1,
+                   "g2": gimbal2,
+                   "g3": gimbal3,
+                   "g4": gimbal4,
+                   "a1": shoulder,
+                   "a2": elbow,
+                   "a3": grabber,
+                   "a4": waist,
+                   "p1": cargo,
+                   }
+
+null_map = {"w1": 0,
+            "w2": 0,
+            "w3": 0,
+            "w4": 0,
+            "w5": 0,
+            "w6": 0,
+            "g1": 90,
+            "g2": 90,
+            "g3": 90,
+            "g4": 90,
+            "a1": 180,
+            "a2": 0,
+            "a3": 0,
+            }
 
 print("Starting driver")
 drive = driver(MODE)
