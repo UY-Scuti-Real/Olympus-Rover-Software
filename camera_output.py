672f9b9a4844db8bdc3e397b8aa58e720eb4a457
UY-Scuti-Real <37155328+UY-Scuti-Real@users.noreply.github.com>
2019-07-01 18:54:31 +0100

    -camera output test

--- camera_output.py
+++ camera_output.py
@@ -1,5 +1,4 @@
 # camera output
-
 """
 doing this is going to be interesting. many options. easiest
 is a tkinter/pygame front end that just updates the image as it comes.
@@ -7,3 +6,12 @@ This is a dangerous approach, though, because tkinter doesn't play
 well with cleanup. Pygame, however, deals with it quite well, but requires
 setup...
 """
+import network_module
+
+server = network_module.make_server(5002, '192.168.1.9')
+server.get_connection()
+while 1:
+    img_str = server.get_messages()
+    file = open("output_test.png", "wb")
+    file.write(img_str.encode())
+    file.close()
